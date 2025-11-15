11703198ef9628748936fd30147a111b54501d2b
Joseph Mead <mead@bnl.gov>
2025-11-15 08:23:11 -0500

    Instantiate 2 BPC's

--- iocBoot/iocbpc/st.cmd
+++ iocBoot/iocbpc/st.cmd
@@ -16,12 +16,15 @@ var("PSCDebug", "5")
 # Listen on 0.0.0.0:1234  (pass zero for random port)
 # for messages coming from "device" localhost:5000
 
-
 createPSCUDP("pscudp1", "10.0.142.199", 5000, 1234)
-#createPSCUDP("pscudp2", "192.168.1.89", 5000, 1235)
+createPSCUDP("pscudp2", "10.0.142.187", 5000, 1235)
 
 ## Load record instances
 dbLoadRecords("db/bpc.db","P=$(IOCNAME), NO=1")
+dbLoadRecords("db/bpc.db","P=$(IOCNAME), NO=2")
+
+
+
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
